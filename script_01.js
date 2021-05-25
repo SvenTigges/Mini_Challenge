35bfb28c934f6e2104015e2085c20ac7d14ae834
SvenTigges <sven.tigges@web.de>
2021-05-25 15:05:01 +0200

    done

--- script_01.js
+++ script_01.js
@@ -10,7 +10,51 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-// ausgabe(getSentence(["Ich","bin","Peter"],"S"));
-// ausgabe(getSentence(["Bist","du","Peter"],"Q"));
-// ausgabe(getSentence(["Ich","bin"],"E"));
-// ausgabe(getSentence(["Ich","bin"],"#"));
+ausgabe(getSentence(["Ich","bin","Peter"],"S"));
+ausgabe(getSentence(["Bist","du","Peter"],"Q"));
+ausgabe(getSentence(["Ich","bin"],"E"));
+ausgabe(getSentence(["Ich","bin"],"#"));
+
+
+function getSentence(arr,ending)
+{   let str = "";  
+    let gap = " ";
+ 
+    
+    for (let i = 0; i < arr.length; i++) 
+    {
+        if (i != arr.length - 1) 
+        {
+          str += arr[i] + gap;
+        }
+         else 
+
+        {
+            switch (ending)
+    {
+                case "S":
+                str += arr[i] + ".";
+                break;
+                case "Q":
+                str += arr[i] + "?";
+                break;
+                case "E":
+                str += arr[i] + "!";
+                break;
+                default:
+                str = "Bitte Satzzeichen eingeben"; 
+                break;
+               
+            }        
+        }
+        
+    }   
+    return str;
+}
+
+//ausgabe("test!");  //check
+function ausgabe(str) 
+{
+    console.log(str);    
+}
+
